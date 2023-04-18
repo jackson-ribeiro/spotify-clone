c016ccc4fcb9c79f27e3b8be84dbb21b8afa98ca
jackson-ribeiro <jacksonquest1@gmail.com>
2023-04-17 22:53:17 -0300

    style: estilização da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import {Home as HomeIcon, Search, Library } from 'lucide-react'
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react'
+import Image from 'next/image'
 
 
 export default function Home() {
@@ -32,7 +33,43 @@ export default function Home() {
           </nav>
         </aside>
         <main className = "flex-1 p-6">
-          main
+          <div className="flex items-center gap-4">
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronLeft />
+            </button>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="font-bold text-4xl mt-10">Boa Noite</h1>
+
+          <div className='grid grid-cols-3 gap-4 mt-4'>
+            <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <Image src= "/albumColdplay.jpg" width={110} height={110} alt='Álbum da banda Coldplay' />
+              <strong>Music Of The Spheres</strong>
+            </a>
+            <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <Image src= "/albumColdplay2.jpg" width={110} height={110} alt='Álbum da banda Coldplay' />
+              <strong>Parachutes</strong>
+            </a>
+            <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <Image src= "/albumColdplay3.jpg" width={110} height={110} alt='Álbum da banda Coldplay' />
+              <strong>Ghost Stories</strong>
+            </a>
+            <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <Image src= "/albumColdplay4.jpg" width={110} height={110} alt='Álbum da banda Coldplay' />
+              <strong>A Head Full Of Dreams</strong>
+            </a>
+            <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <Image src= "/albumColdplay5.jpg" width={110} height={110} alt='Álbum da banda Coldplay' />
+              <strong>A Rush of Blood to the Head</strong>
+            </a>
+            <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <Image src= "/albumColdplay6.jpg" width={110} height={110} alt='Álbum da banda Coldplay' />
+              <strong>Mylo Xyoto</strong>
+            </a>
+          </div>
         </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
