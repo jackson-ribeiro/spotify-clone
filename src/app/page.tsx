1e1279a9be475c900a45a7a163729e5f7c0d2745
Jackson Ribeiro de Oliveira Filho <jacksonfilho@deso-se.com.br>
2023-04-28 14:05:55 -0300

    style: ícone e footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react'
 import Image from 'next/image'
 
 
@@ -116,13 +116,40 @@ export default function Home() {
               <strong className="font-semibold">Daily mix 1</strong>
               <span className="text-xs text-zinc-500">Wallows, COIN, girl in red and more</span>
             </a>
-            
           </div>
-
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/albumColdplay.jpg" width={56} height={56} alt="Álbum da banda Coldplay" />
+          <div className="flex flex-col">
+            <strong className="font-normal">Higher Power</strong>
+            <span className="text-xs text-zinc-400">Coldplay</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <Shuffle  size = {20} className="text-zinc-200" />
+            <SkipBack size = {20} className="text-zinc-200" />
+
+            <button className ="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <Play />
+            </button>
+
+            <SkipForward size = {20} className="text-zinc-200" />
+            <Repeat size = {20} className="text-zinc-200" />
+          </div>
+          <div className = "flex items-center gap-2">
+            <span className = "text-xs text-zinc-400">1:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className = "text-xs text-zinc-400">3:26</span>
+          </div>
+        </div>
+        <div>
+
+        </div>
       </footer>
     </div>
   )
